ea4792bb3e9bded92b3dd39761483a096e70b40f
khw <hyunwook711@naver.com>
2021-03-20 15:29:13 +0900

    first commit

---
+++ AutoConnect.py
@@ -0,0 +1,15 @@
+gotfrom pywinauto import application
+import time
+import os
+
+os.system('taskkill /IM coStarter* /F /T')
+os.system('taskkill /IM CpStart* /F /T')
+os.system('taskkill /IM DibServer* /F /T')
+os.system('wmic process where "name like \'%coStarter%\'" call terminate')
+os.system('wmic process where "name like \'%CpStart%\'" call terminate')
+os.system('wmic process where "name like \'%DibServer%\'" call terminate')
+time.sleep(5)        
+
+app = application.Application()
+app.start('C:\CREON\STARTER\coStarter.exe /prj:cp /id:hwven /pwd:w@@k5648 /pwdcert:gusw00k5648@@ /autostart')
+time.sleep(60)
